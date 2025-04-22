45a67225ed671d221b545b1d8c4c1d5e2e2a1baa
HEMANTHSAI2005 <163678484+HEMANTHSAI2005@users.noreply.github.com>
2025-04-22 11:00:46 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,56 +1,57 @@
-import React, { useContext } from 'react'
-import { useEffect } from 'react'
-import { Route, Routes } from 'react-router-dom'
-import Applications from './pages/Applications'
-import Applyjob from './pages/Applyjob'
-import Home from './pages/Home'
-import RecruiterLogin from './components/RecruiterLogin'
-import { AppContext } from './context/AppContext'
-import Dashboard from './pages/Dashboard'
-import AddJob from './pages/AddJob'
-import ManageJobs from './pages/ManageJobs'
-import ViewApplications from './pages/ViewApplications'
-import 'quill/dist/quill.snow.css'
+import React, { useContext, useEffect } from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Applications from './pages/Applications';
+import Applyjob from './pages/Applyjob';
+import Home from './pages/Home';
+import RecruiterLogin from './components/RecruiterLogin';
+import { AppContext } from './context/AppContext';
+import Dashboard from './pages/Dashboard';
+import AddJob from './pages/AddJob';
+import ManageJobs from './pages/ManageJobs';
+import ViewApplications from './pages/ViewApplications';
+import 'quill/dist/quill.snow.css';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import { ClerkProvider } from '@clerk/clerk-react';
 
+// Add the Clerk provider and your environment variable
 const App = () => {
+  const { showRecruiterLogin, companyToken } = useContext(AppContext);
 
-  const {showRecruiterLogin, companyToken} = useContext(AppContext)
-
+  // Log the environment variable to make sure it's being used
   console.log("Backend URL test:", import.meta.env.VITE_BACKEND_URL);
+  console.log("Clerk Publishable Key:", import.meta.env.VITE_CLERK_PUBLISHABLE_KEY);
 
-  // useEffect(() => {
-  //   fetch(import.meta.env.VITE_BACKEND_URL+'/api/jobs')
-  //     .then(res => res.json())
-  //     .then(data => console.log(" Test Fetch Response:", data))
-  //     .catch(err => console.error("Test Fetch Error:", err));
-  // }, []);
+  // Initialize ClerkProvider with the publishable key
+  const clerkFrontendApi = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 
-  
   return (
+    <ClerkProvider frontendApi={clerkFrontendApi}>
+      <div>
+        {/* Show Recruiter Login if true */}
+        {showRecruiterLogin && <RecruiterLogin />}
 
-    <div>
-      { showRecruiterLogin && <RecruiterLogin />}
+        {/* Display Toast Notifications */}
+        <ToastContainer />
 
-      <ToastContainer />
+        {/* Define Routes */}
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/apply-job/:id" element={<Applyjob />} />
+          <Route path="/applications" element={<Applications />} />
+          <Route path="/dashboard" element={<Dashboard />}>
+            {companyToken ? (
+              <>
+                <Route path="add-job" element={<AddJob />} />
+                <Route path="manage-jobs" element={<ManageJobs />} />
+                <Route path="view-applications" element={<ViewApplications />} />
+              </>
+            ) : null}
+          </Route>
+        </Routes>
+      </div>
+    </ClerkProvider>
+  );
+};
 
-      <Routes>
-        <Route path = '/' element = {<Home />}/> 
-        <Route path = '/apply-job/:id' element = {<Applyjob />}/> 
-        <Route path = '/applications' element = {<Applications />}/> 
-        <Route path = '/dashboard' element={<Dashboard/>}>
-        { companyToken ? <>
-          <Route path = 'add-job' element={<AddJob/>}/> 
-          <Route path = 'manage-jobs' element={<ManageJobs/>}/> 
-          <Route path = 'view-applications' element={<ViewApplications/>}/> 
-
-        </> : null}
-          
-        </Route>
-      </Routes>
-    </div>
-  )
-}
-
-export default App
+export default App;

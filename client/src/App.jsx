cd2bceb9bd2eb68326c38f1b060c3f9d7a6410ee
HEMANTHSAI2005 <163678484+HEMANTHSAI2005@users.noreply.github.com>
2025-04-22 13:47:34 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,112 +1,112 @@
-// import React, { useContext } from 'react'
-// import { useEffect } from 'react'
-// import { Route, Routes } from 'react-router-dom'
-// import Applications from './pages/Applications'
-// import Applyjob from './pages/Applyjob'
-// import Home from './pages/Home'
-// import RecruiterLogin from './components/RecruiterLogin'
-// import { AppContext } from './context/AppContext'
-// import Dashboard from './pages/Dashboard'
-// import AddJob from './pages/AddJob'
-// import ManageJobs from './pages/ManageJobs'
-// import ViewApplications from './pages/ViewApplications'
-// import 'quill/dist/quill.snow.css'
-// import { ToastContainer, toast } from 'react-toastify';
-// import 'react-toastify/dist/ReactToastify.css';
-
-// const App = () => {
-
-//   const {showRecruiterLogin, companyToken} = useContext(AppContext)
-
-//   console.log("Backend URL test:", import.meta.env.VITE_BACKEND_URL);
-
-//   // useEffect(() => {
-//   //   fetch(import.meta.env.VITE_BACKEND_URL+'/api/jobs')
-//   //     .then(res => res.json())
-//   //     .then(data => console.log(" Test Fetch Response:", data))
-//   //     .catch(err => console.error("Test Fetch Error:", err));
-//   // }, []);
-
-  
-//   return (
-
-//     <div>
-//       { showRecruiterLogin && <RecruiterLogin />}
-
-//       <ToastContainer />
-
-//       <Routes>
-//         <Route path = '/' element = {<Home />}/> 
-//         <Route path = '/apply-job/:id' element = {<Applyjob />}/> 
-//         <Route path = '/applications' element = {<Applications />}/> 
-//         <Route path = '/dashboard' element={<Dashboard/>}>
-//         { companyToken ? <>
-//           <Route path = 'add-job' element={<AddJob/>}/> 
-//           <Route path = 'manage-jobs' element={<ManageJobs/>}/> 
-//           <Route path = 'view-applications' element={<ViewApplications/>}/> 
-
-//         </> : null}
-          
-//         </Route>
-//       </Routes>
-//     </div>
-//   )
-// }
-
-// export default App
-
-
-
-
-
-
-
-
-import React, { useContext } from 'react';
-import { Route, Routes } from 'react-router-dom';
-import { ClerkProvider } from '@clerk/clerk-react'; // Import ClerkProvider
-import Applications from './pages/Applications';
-import Applyjob from './pages/Applyjob';
-import Home from './pages/Home';
-import RecruiterLogin from './components/RecruiterLogin';
-import { AppContext } from './context/AppContext';
-import Dashboard from './pages/Dashboard';
-import AddJob from './pages/AddJob';
-import ManageJobs from './pages/ManageJobs';
-import ViewApplications from './pages/ViewApplications';
-import 'quill/dist/quill.snow.css';
-import { ToastContainer } from 'react-toastify';
+import React, { useContext } from 'react'
+import { useEffect } from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Applications from './pages/Applications'
+import Applyjob from './pages/Applyjob'
+import Home from './pages/Home'
+import RecruiterLogin from './components/RecruiterLogin'
+import { AppContext } from './context/AppContext'
+import Dashboard from './pages/Dashboard'
+import AddJob from './pages/AddJob'
+import ManageJobs from './pages/ManageJobs'
+import ViewApplications from './pages/ViewApplications'
+import 'quill/dist/quill.snow.css'
+import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 const App = () => {
-  const { showRecruiterLogin, companyToken } = useContext(AppContext);
 
-  // Your Clerk Frontend API URL
-  const frontendApi = 'https://awake-starling-80.clerk.accounts.dev';
+  const {showRecruiterLogin, companyToken} = useContext(AppContext)
 
+  console.log("Backend URL test:", import.meta.env.VITE_BACKEND_URL);
+
+  // useEffect(() => {
+  //   fetch(import.meta.env.VITE_BACKEND_URL+'/api/jobs')
+  //     .then(res => res.json())
+  //     .then(data => console.log(" Test Fetch Response:", data))
+  //     .catch(err => console.error("Test Fetch Error:", err));
+  // }, []);
+
+  
   return (
-    <ClerkProvider frontendApi={frontendApi}>
-      <div>
-        {showRecruiterLogin && <RecruiterLogin />}
-        <ToastContainer />
 
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/apply-job/:id" element={<Applyjob />} />
-          <Route path="/applications" element={<Applications />} />
-          <Route path="/dashboard" element={<Dashboard />}>
-            {companyToken ? (
-              <>
-                <Route path="add-job" element={<AddJob />} />
-                <Route path="manage-jobs" element={<ManageJobs />} />
-                <Route path="view-applications" element={<ViewApplications />} />
-              </>
-            ) : null}
-          </Route>
-        </Routes>
-      </div>
-    </ClerkProvider>
-  );
-};
+    <div>
+      { showRecruiterLogin && <RecruiterLogin />}
 
-export default App;
+      <ToastContainer />
+
+      <Routes>
+        <Route path = '/' element = {<Home />}/> 
+        <Route path = '/apply-job/:id' element = {<Applyjob />}/> 
+        <Route path = '/applications' element = {<Applications />}/> 
+        <Route path = '/dashboard' element={<Dashboard/>}>
+        { companyToken ? <>
+          <Route path = 'add-job' element={<AddJob/>}/> 
+          <Route path = 'manage-jobs' element={<ManageJobs/>}/> 
+          <Route path = 'view-applications' element={<ViewApplications/>}/> 
+
+        </> : null}
+          
+        </Route>
+      </Routes>
+    </div>
+  )
+}
+
+export default App
+
+
+
+
+
+
+
+
+// import React, { useContext } from 'react';
+// import { Route, Routes } from 'react-router-dom';
+// import { ClerkProvider } from '@clerk/clerk-react'; // Import ClerkProvider
+// import Applications from './pages/Applications';
+// import Applyjob from './pages/Applyjob';
+// import Home from './pages/Home';
+// import RecruiterLogin from './components/RecruiterLogin';
+// import { AppContext } from './context/AppContext';
+// import Dashboard from './pages/Dashboard';
+// import AddJob from './pages/AddJob';
+// import ManageJobs from './pages/ManageJobs';
+// import ViewApplications from './pages/ViewApplications';
+// import 'quill/dist/quill.snow.css';
+// import { ToastContainer } from 'react-toastify';
+// import 'react-toastify/dist/ReactToastify.css';
+
+// const App = () => {
+//   const { showRecruiterLogin, companyToken } = useContext(AppContext);
+
+//   // Your Clerk Frontend API URL
+//   const frontendApi = 'https://awake-starling-80.clerk.accounts.dev';
+
+//   return (
+//     <ClerkProvider frontendApi={frontendApi}>
+//       <div>
+//         {showRecruiterLogin && <RecruiterLogin />}
+//         <ToastContainer />
+
+//         <Routes>
+//           <Route path="/" element={<Home />} />
+//           <Route path="/apply-job/:id" element={<Applyjob />} />
+//           <Route path="/applications" element={<Applications />} />
+//           <Route path="/dashboard" element={<Dashboard />}>
+//             {companyToken ? (
+//               <>
+//                 <Route path="add-job" element={<AddJob />} />
+//                 <Route path="manage-jobs" element={<ManageJobs />} />
+//                 <Route path="view-applications" element={<ViewApplications />} />
+//               </>
+//             ) : null}
+//           </Route>
+//         </Routes>
+//       </div>
+//     </ClerkProvider>
+//   );
+// };
+
+// export default App;
